3317c14ca885ae44f25d045f0e120d9a551df2a9
wsn <162379510+wsn0@users.noreply.github.com>
2024-03-06 06:30:32 +0700

    c# source code ควบคุมตัวละคร

---
+++ charcontrol.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charcontrol : MonoBehaviour
+{
+    public float speed = 0;
+    public AudioSource audioSource;
+    public AudioClip jumpsfx;
+    private Animator myAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            //transform.position += Vector3.forward * speed * Time.deltaTime;
+            myAnimator.Play("forward");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            //transform.position += Vector3.left * speed * Time.deltaTime;
+            myAnimator.Play("turnL");
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            //transform.position += Vector3.back * speed * Time.deltaTime;
+            myAnimator.Play("back");
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            //transform.position += Vector3.right * speed * Time.deltaTime;
+            myAnimator.Play("turnR");
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+            audioSource.PlayOneShot(jumpsfx);
+            myAnimator.Play("jumping");
+        }
+    }
+}
