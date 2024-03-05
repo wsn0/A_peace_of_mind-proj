f44c9d11444d75e044bc564c6f5bd97d3266d035
wsn <162379510+wsn0@users.noreply.github.com>
2024-03-06 06:28:49 +0700

    c# source code ควบคุม menu

---
+++ Menucontroller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menucontroller : MonoBehaviour
+{
+    public void changescence(string scenceName)
+    {
+        SceneManager.LoadScene(scenceName);
+    }
+
+    public void exitgame()
+    {
+        Application.Quit();
+    }
+}
