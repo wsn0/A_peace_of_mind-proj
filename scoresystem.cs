5e3bf56197a9f0e7c1f56d3de4af6d6a3eb8cd25
wsn <162379510+wsn0@users.noreply.github.com>
2024-03-06 06:31:39 +0700

    c# source code แต้มในเกม

---
+++ scoresystem.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoresystem : MonoBehaviour
+{
+    public int score = 0;
+    public Text Scoretext;
+    public AudioSource biting;
+    public AudioSource miss;
+    public AudioSource flushing;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider item)
+    {
+        if(item.tag == "onepoint")
+        {
+            biting.Play();
+            score = score + 1;
+            Scoretext.text = "SCORE :    " + score;
+            Destroy(item.gameObject);
+        }
+        if (item.tag == "minusone")
+        {
+            miss.Play();
+            score = score - 1;
+            Scoretext.text = "SCORE :    " + score;
+            Destroy(item.gameObject);
+        }
+        if (item.tag == "minustwo")
+        {
+            miss.Play();
+            score = score - 2;
+            Scoretext.text = "SCORE :    " + score;
+            Destroy(item.gameObject);
+        }
+        if (item.tag == "fivepoint")
+        {
+            biting.Play();
+            score = score + 5;
+            Scoretext.text = "SCORE :    " + score;
+            Destroy(item.gameObject);
+            flushing.PlayDelayed(1);
+
+        }
+        if (item.tag == "twopoint")
+        {
+            biting.Play();
+            score = score + 2;
+            Scoretext.text = "SCORE :    " + score;
+            Destroy(item.gameObject);
+        }
+    }
+}
